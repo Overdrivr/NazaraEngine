51647e8a3e5de8e8091ade851244ede7640d2de7
Rémi Bèges <remi.beges@gmail.com>
2015-08-04 11:48:43 +0200

    Added noise tools

---
+++ include/Nazara/Noise/NoiseTools.hpp
@@ -0,0 +1,15 @@
+// Copyright (C) 2015 Rémi Bèges
+// This file is part of the "Nazara Engine - Noise module"
+// For conditions of distribution and use, see copyright notice in Config.hpp
+
+#pragma once
+
+#ifndef NOISETOOLS_HPP
+#define NOISETOOLS_HPP
+
+#include <Nazara/Prerequesites.hpp>
+
+int fastfloor(float n);
+int JenkinsHash(int a, int b, int c);
+
+#endif // NOISETOOLS_HPP
